caa97020e5bbb836e2e8d3c2ad7219f09978f67d
jiangmengqi <jmqrock@hotmail.com>
2024-02-12 11:42:09 +0800

    update lazygit conf; fix pig autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,8 +2,17 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local src_pig_syntax = "source" .. os.getenv("HOME") .. "/.config/nvim/syntax/pig.vim"
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.pig" },
-  command = src_pig_syntax,
+  callback = function()
+    local src_pig_syntax ="source " .. vim.fn.expand("~") .. "/.config/nvim/syntax/pig.vim"
+    vim.cmd(src_pig_syntax)
+  end,
 })
+
+-- for multi autocmds
+-- for event, opt_tbls in pairs(auto_cmd_dict) do
+--   for _, opt_tbl in pairs(opt_tbls) do
+--     vim.api.nvim_create_autocmd(event, opt_tbl)
+--   end
+-- end
