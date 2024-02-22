60f995dad696c3322435f668b39a821b46e93fbb
Karas Jiang <jmqrock@hotmail.com>
2024-02-22 17:05:59 +0800

    update tab and status conf

--- wezterm/status.lua
+++ wezterm/status.lua
@@ -3,73 +3,90 @@
 --
 local wezterm = require('wezterm')
 local icons = wezterm.nerdfonts
-local hostname = wezterm.hostname()
+local is_studio = string.find(wezterm.hostname(), 'Studio') ~= nil
+local tilde = ''
 
 local M = {}
+
+local function apply_fmt(fg, bg, appled_str)
+  local fmt_array = {}
+  if fg then
+    table.insert(fmt_array, { Foreground = { AnsiColor = fg } })
+  end
+
+  if bg then
+    table.insert(fmt_array, { Background = { AnsiColor = bg } })
+  end
+  table.insert(fmt_array, { Text = appled_str })
+
+  return wezterm.format(fmt_array)
+end
 
 --- @param battery battery state of charge, from 0.00 to 1.00
 --- @return string
 local function render_battery(battery)
   --- @type table<string, string>
 
-  if string.find(hostname, 'Studio') ~= nil then
-    return wezterm.format({
-      { Foreground = { AnsiColor = 'Green' } },
-      -- { Text = icons['md_desktop_mac'] },
-      { Text = icons['cod_plug'] },
-    })
+  if is_studio then
+    return string.format(
+      '%s%s',
+      apply_fmt('Green', nil, tilde),
+      apply_fmt('Black', 'Green', string.format(' %s ', icons['cod_plug']))
+    )
   end
 
   local percent = battery.state_of_charge
   local formatted_percent = string.format('%.0f%%', percent * 100)
 
-  local get_icon = function()
-    local prefix = 'md_battery'
-    if battery.state == 'Charging' then
-      return icons[prefix .. '_charging']
-    end
-
-    local icon_name
-
-    if percent == 1 then
-      icon_name = 'md_battery_charging_100'
-    else
-      local suffix = math.max(1, math.ceil(percent * 10)) .. '0'
-      icon_name = prefix .. '_' .. suffix
-    end
-
-    local color = percent <= 0.1 and 'Red' or 'Green'
-    return wezterm.format({
-      { Foreground = { AnsiColor = color } },
-      { Text = icons[icon_name] },
-    })
+  local prefix = 'md_battery'
+  if battery.state == 'Charging' then
+    return icons[prefix .. '_charging']
   end
 
-  return string.format('%s %s', get_icon(), formatted_percent)
+  local icon_name
+
+  if percent == 1 then
+    icon_name = 'md_battery_charging_100'
+  else
+    local suffix = math.max(1, math.ceil(percent * 10)) .. '0'
+    icon_name = prefix .. '_' .. suffix
+  end
+
+  local color = percent <= 0.1 and 'Red' or 'Green'
+  return string.format(
+    '%s%s',
+    apply_fmt('Green', nil, tilde),
+    apply_fmt(color, 'Green', string.format(' %s%s ', icons[icon_name], formatted_percent))
+  )
 end
 
 local function get_mac_icon()
   local mac_icon = icons['md_laptop']
-  if string.find(hostname, 'Studio') ~= nil then
-    mac_icon = icons['md_desktop_mac']
+  if is_studio then
+    mac_icon = icons['md_slot_machine_outline']
   end
-  return wezterm.format({
-    { Foreground = { AnsiColor = 'Green' } },
-    { Text = mac_icon },
-  })
+  return string.format(
+    '%s%s',
+    apply_fmt('Yellow', 'Silver', tilde),
+    apply_fmt('Black', 'Yellow', string.format(' %s ', mac_icon))
+  )
 end
 
 local function update_right_status(window)
   -- "Mar 3[Wed] 08:14"
   --- @type string
-  local date = wezterm.strftime('%b %-d[%a] %H:%M')
+  local date = string.format(
+    '%s%s',
+    apply_fmt('Silver', 'Green', tilde),
+    apply_fmt('Black', 'Silver', string.format(' %s ', wezterm.strftime('%b %-d[%a] %H:%M')))
+  )
 
-  --- @type string
-  local tilde = wezterm.format({
-    { Foreground = { AnsiColor = 'Fuchsia' } },
-    { Text = '|' },
-  })
-
+  -- --- @type string
+  -- local tilde = wezterm.format({
+  --   { Foreground = { AnsiColor = 'Fuchsia' } },
+  --   { Text = icons['cod_triangle_left'] },
+  -- })
+  --
   --- @type string
   local battery
 
@@ -78,7 +95,7 @@ local function update_right_status(window)
   end
 
   window:set_right_status(wezterm.format({
-    { Text = string.format('%s %s%s %s%s ', battery, tilde, date, tilde, get_mac_icon()) },
+    { Text = string.format('%s%s%s', battery, date, get_mac_icon()) },
   }))
 end
 
--- wezterm/tab.lua
+++ wezterm/tab.lua
@@ -1,166 +1,171 @@
 local wezterm = require('wezterm')
+local icons = wezterm.nerdfonts
 local M = {}
 
 -- This function is private to this module and is not visible
 -- outside.
 local function private_helper()
-   wezterm.log_info('load tab config!')
+  wezterm.log_info('load tab config!')
 end
 
-local GLYPH_SEMI_CIRCLE_LEFT = ''
+local GLYPH_SEMI_CIRCLE_LEFT = icons['ple_lower_right_triangle']
+-- local GLYPH_SEMI_CIRCLE_LEFT = ''
 -- local GLYPH_SEMI_CIRCLE_LEFT = utf8.char(0xe0b6)
-local GLYPH_SEMI_CIRCLE_RIGHT = ''
+local GLYPH_SEMI_CIRCLE_RIGHT = icons['ple_upper_left_triangle']
+-- local GLYPH_SEMI_CIRCLE_RIGHT = ''
 -- local GLYPH_SEMI_CIRCLE_RIGHT = utf8.char(0xe0b4)
-local GLYPH_CIRCLE = ''
+local GLYPH_CIRCLE = icons['cod_eye']
+-- local GLYPH_CIRCLE = ''
 -- local GLYPH_CIRCLE = utf8.char(0xf111)
-local GLYPH_ADMIN = '󰞀'
+local GLYPH_ADMIN = icons['fae_radioactive']
+-- local GLYPH_ADMIN = '󰞀'
 -- local GLYPH_ADMIN = utf8.char(0xf0780)
 
 M.cells = {}
 
 M.colors = {
-   default = {
-      bg = '#45475a',
-      fg = '#1c1b19',
-   },
-   is_active = {
-      bg = '#7FB4CA',
-      fg = '#11111b',
-   },
+  default = {
+    bg = { AnsiColor = 'Green' },
+    fg = { AnsiColor = 'Black' },
+  },
+  is_active = {
+    bg = { AnsiColor = 'Yellow' },
+    fg = { AnsiColor = 'Black' },
+  },
 
-   hover = {
-      bg = '#587d8c',
-      fg = '#1c1b19',
-   },
+  hover = {
+    bg = { AnsiColor = 'Red' },
+    fg = { AnsiColor = 'Black' },
+  },
 }
 
 M.set_process_name = function(s)
-   local a = string.gsub(s, '(.*[/\\])(.*)', '%2')
-   return a:gsub('%.exe$', '')
+  local a = string.gsub(s, '(.*[/\\])(.*)', '%2')
+  return a:gsub('%.exe$', '')
 end
 
 M.set_title = function(process_name, base_title, max_width, inset)
-   local title
-   inset = inset or 6
+  local title
+  inset = inset or 6
 
-   if process_name:len() > 0 then
-      title = process_name .. '⏽' .. base_title
-   else
-      title = base_title
-   end
+  if process_name:len() > 0 then
+    title = process_name .. '⏽' .. base_title
+  else
+    title = base_title
+  end
 
-   if title:len() > max_width - inset then
-      local diff = title:len() - max_width + inset
-      title = wezterm.truncate_right(title, title:len() - diff)
-   end
+  if title:len() > max_width - inset then
+    local diff = title:len() - max_width + inset
+    title = wezterm.truncate_right(title, title:len() - diff)
+  end
 
-   return title
+  return title
 end
 
 M.check_if_admin = function(p)
-   if p:match('^Administrator: ') then
-      return true
-   end
-   return false
+  if p:match('^Administrator: ') then
+    return true
+  end
+  return false
 end
 
----@param fg string
----@param bg string
+---@param fg table
+---@param bg table
 ---@param attribute table
 ---@param text string
 M.push = function(bg, fg, attribute, text)
-   table.insert(M.cells, { Background = { Color = bg } })
-   table.insert(M.cells, { Foreground = { Color = fg } })
-   table.insert(M.cells, { Attribute = attribute })
-   table.insert(M.cells, { Text = text })
+  table.insert(M.cells, { Background = bg })
+  table.insert(M.cells, { Foreground = fg })
+  table.insert(M.cells, { Attribute = attribute })
+  table.insert(M.cells, { Text = text })
 end
 
 function M.apply(config)
-   config.enable_tab_bar = true
-   config.use_fancy_tab_bar = false
-   config.tab_bar_at_bottom = true
-   config.hide_tab_bar_if_only_one_tab = false
-   config.use_fancy_tab_bar = false
-   config.tab_max_width = 30
-   config.show_tab_index_in_tab_bar = true
-   config.switch_to_last_active_tab_when_closing_tab = true
+  config.enable_tab_bar = true
+  config.use_fancy_tab_bar = false
+  config.tab_bar_at_bottom = true
+  config.hide_tab_bar_if_only_one_tab = false
+  config.use_fancy_tab_bar = false
+  config.tab_max_width = 30
+  config.show_tab_index_in_tab_bar = true
+  config.switch_to_last_active_tab_when_closing_tab = true
 
-   config.colors = {
-      tab_bar = {
-         background = '#000000',
-      },
-   }
+  config.colors = {
+    tab_bar = {
+      background = 'Black',
+    },
+  }
 
-   wezterm.on('new-tab-button-click', function(window, pane, button, default_action)
-      wezterm.log_info('new-tab', window, pane, button, default_action)
-      if default_action and button == 'Left' then
-         window:perform_action(default_action, pane)
+  wezterm.on('new-tab-button-click', function(window, pane, button, default_action)
+    wezterm.log_info('new-tab', window, pane, button, default_action)
+    if default_action and button == 'Left' then
+      window:perform_action(default_action, pane)
+    end
+
+    if default_action and button == 'Right' then
+      window:perform_action(
+        wezterm.action.ShowLauncherArgs({
+          title = '  Select/Search:',
+          flags = 'FUZZY|LAUNCH_MENU_ITEMS|DOMAINS',
+        }),
+        pane
+      )
+    end
+    return false
+  end)
+
+  wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    M.cells = {}
+
+    local bg
+    local fg
+    local process_name = M.set_process_name(tab.active_pane.foreground_process_name)
+    local is_admin = M.check_if_admin(tab.active_pane.title)
+    local title = M.set_title(process_name, tab.active_pane.title, max_width, (is_admin and 8))
+
+    if tab.is_active then
+      bg = M.colors.is_active.bg
+      fg = M.colors.is_active.fg
+    elseif hover then
+      bg = M.colors.hover.bg
+      fg = M.colors.hover.fg
+    else
+      bg = M.colors.default.bg
+      fg = M.colors.default.fg
+    end
+
+    local has_unseen_output = false
+    for _, pane in ipairs(tab.panes) do
+      if pane.has_unseen_output then
+        has_unseen_output = true
+        break
       end
+    end
 
-      if default_action and button == 'Right' then
-         window:perform_action(
-            wezterm.action.ShowLauncherArgs({
-               title = '  Select/Search:',
-               flags = 'FUZZY|LAUNCH_MENU_ITEMS|DOMAINS',
-            }),
-            pane
-         )
-      end
-      return false
-   end)
+    -- Left semi-circle
+    M.push(fg, bg, { Intensity = 'Normal' }, GLYPH_SEMI_CIRCLE_LEFT)
 
-   wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-      M.cells = {}
+    -- Admin Icon
+    if is_admin then
+      M.push(bg, fg, { Intensity = 'Normal' }, ' ' .. GLYPH_ADMIN)
+    end
 
-      local bg
-      local fg
-      local process_name = M.set_process_name(tab.active_pane.foreground_process_name)
-      local is_admin = M.check_if_admin(tab.active_pane.title)
-      local title = M.set_title(process_name, tab.active_pane.title, max_width, (is_admin and 8))
+    -- Title
+    M.push(bg, { Color = 'Black' }, { Intensity = 'Bold' }, ' ' .. title)
 
-      if tab.is_active then
-         bg = M.colors.is_active.bg
-         fg = M.colors.is_active.fg
-      elseif hover then
-         bg = M.colors.hover.bg
-         fg = M.colors.hover.fg
-      else
-         bg = M.colors.default.bg
-         fg = M.colors.default.fg
-      end
+    -- Unseen output alert
+    if has_unseen_output then
+      M.push(bg, { Color = '#FFA066' }, { Intensity = 'Normal' }, ' ' .. GLYPH_CIRCLE)
+    end
 
-      local has_unseen_output = false
-      for _, pane in ipairs(tab.panes) do
-         if pane.has_unseen_output then
-            has_unseen_output = true
-            break
-         end
-      end
+    -- Right padding
+    M.push(bg, fg, { Intensity = 'Bold' }, ' ')
 
-      -- Left semi-circle
-      M.push(fg, bg, { Intensity = 'Bold' }, GLYPH_SEMI_CIRCLE_LEFT)
+    -- Right semi-circle
+    M.push(fg, bg, { Intensity = 'Bold' }, GLYPH_SEMI_CIRCLE_RIGHT)
 
-      -- Admin Icon
-      if is_admin then
-         M.push(bg, fg, { Intensity = 'Bold' }, ' ' .. GLYPH_ADMIN)
-      end
-
-      -- Title
-      M.push(bg, fg, { Intensity = 'Bold' }, ' ' .. title)
-
-      -- Unseen output alert
-      if has_unseen_output then
-         M.push(bg, '#FFA066', { Intensity = 'Bold' }, ' ' .. GLYPH_CIRCLE)
-      end
-
-      -- Right padding
-      M.push(bg, fg, { Intensity = 'Bold' }, ' ')
-
-      -- Right semi-circle
-      M.push(fg, bg, { Intensity = 'Bold' }, GLYPH_SEMI_CIRCLE_RIGHT)
-
-      return M.cells
-   end)
+    return M.cells
+  end)
 end
 
 return M
